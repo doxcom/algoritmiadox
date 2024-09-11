7e5587bfcccba8d81b72fc67af580b7157d33600
doxcom <aldoperez1319@gmail.com>
2024-09-11 04:12:26 -0600

    if added to compare complemental

--- SolutionTwoSum.java
+++ SolutionTwoSum.java
@@ -1,49 +1,37 @@
-Java
-
+import java.util.HashMap;
 
 class SolutionTwoSum {
     // https://leetcode.com/problems/two-sum/
 
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         int[] sumarray = { 2, 7, 11, 15 };
 
         twoSum(sumarray,9);
-=======
-        int[] sumarray = { 2, 7, 11, 15 }; 
         System.out.println(twoSum(sumarray, 9));
->>>>>>> d3ba349941e403c8d559d7f8f3e4ea4320924f02
     }
-    HashMap<Integer, Integer> indexStore = new HashMap<>();
+
     static int[] twoSum(int[] nums, int target) {
-<<<<<<< HEAD
-
-         for(int i=0; i<nums.length; i++){
-
-         }
-
-
-        return nums;
-    }
-
-=======
+        HashMap<Integer, Integer> indexStore = new HashMap<>();
       int numbCom=0;
-      bool flag=false;
+      boolean flag=false;//to indicate i is looking for j complement
         for (int i = 0; i < nums.length; i++) {
             for (int j = nums.length-1; j <= 0; j--) {
                  i++;
                 if(nums[i]+nums[j]==target){
                 indexStore.put(i,j);
                  break;
-                }else if(nums[i]<target &&   nums[j]>=target){
+                }else if(nums[i]<target && nums[j]>=target && flag==false){
                     indexStore.put(i,-1);
-                  numbCom=target-nums[i];
-                  bool=true;
-               }
+                  numbCom=target-nums[i];//num to find is 7
+                  flag=true;
+               }else if(nums[j]<target && nums[j]>=target && flag==false){
+                    indexStore.put(-1,j);
+
+                }
             }
         }
 
         return indexStore;
->>>>>>> d3ba349941e403c8d559d7f8f3e4ea4320924f02
     }
+}

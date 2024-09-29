c64fe24d7b01e2f25fb75cdf3501dd47c5b9d4cd
Aldo Perez <49726873+doxcom@users.noreply.github.com>
2024-09-28 19:48:45 -0600

    Update to find only 2 numbers

--- SolutionTwoSum.java
+++ SolutionTwoSum.java
@@ -5,35 +5,33 @@ class SolutionTwoSum {
 
     public static void main(String[] args) {
 
-        int[] sumarray = { 3, 3 };
-        //3,2,4  target 6
-
-        twoSum(sumarray,6);
+        int[] sumarray = { 2,7,11,15};
+        twoSum(sumarray,9);
         System.out.println(twoSum(sumarray, 9));
     }
-   // 2, 7, 11, 15   target 9+
+   
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> indexStore = new HashMap<>();
       int numbCom1=-1;
       int numbCom2=-1;
       int [] pos =new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = nums.length-1; j >= 0; j--) {
-              //   i++;
-                 if(nums[i]+nums[j]==target){
-                     pos= new int[]{i,j};
-
-                     System.out.println("position "+ pos[i]+","+pos[j]);
-                   //  return pos;
+        for (int i = 0; i <nums.length; i++) {
+            for (int j<nums.lengthe-1; j>=0; j--){
+              if(indexStore.size(2) && indexStore.get(numbCom1))
+               if(nums[i]<target &&  nums[j]>target){
+                  numbCom1=target-nums[i]; //
+                  indexStore.put(nums[i],i);  //2,0
+                     }else if(nums[j]<target && nums[i]>target){
+                     numbCom2=target-nums[j];
+                     indexStore.put(j,nums[j]); 
+                      }                    
                  }
                 }
             }
-
-      //  System.out.println(pos[0]+","+pos[1]);
+   //  System.out.println(pos[0]+","+pos[1]);
         return pos;
 
         }
 
 
     }
-

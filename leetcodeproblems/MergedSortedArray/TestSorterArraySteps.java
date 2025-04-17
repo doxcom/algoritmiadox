62ef4e615581e5be35becd0a91a4c08dc9bcfe0f
doxcom <aldoperez1319@gmail.com>
2025-04-17 14:16:05 -0600

    arreglando array mezcla

--- leetcodeproblems/MergedSortedArray/TestSorterArraySteps.java
+++ leetcodeproblems/MergedSortedArray/TestSorterArraySteps.java
@@ -1,5 +1,5 @@
 package leetcodeproblems.MergedSortedArray;
-
+// problem https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
 public class TestSorterArraySteps {
 
    public static void  merge(int[] a, int[] b, int m, int n){
@@ -9,18 +9,22 @@ public class TestSorterArraySteps {
     while(nums1Size>0){
      //compare
 
-        if(a[i]<b[j] || a[i]==b[j]){
-            if(a[i]==0){
-                a[i]=b[j];
-            }
+        if(a[i]<b[j]  || a[i]==0){
             i++;
-        }else if(a[i] > b[j]){
+        }else if( a[i]==b[j]){
+            b[j] = a[i+1];
             a[i+1] = a[i];
-            a[i] = b[j];
+            i++;
+        }else if(a[i] > b[j] ){
+
         }
       nums1Size--;
     }
    }
+ // i0  i1  i2
+//   1,  2 , 2  , 0,  0 , 0
+//  j0
+//   3,  5 , 6 ,
 
    public static void main(String[] args) {
 

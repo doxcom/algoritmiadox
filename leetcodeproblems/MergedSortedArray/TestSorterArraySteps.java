020c05d3b62084ef40fc51c74fce38bdcfdb81a3
doxcom <aldoperez1319@gmail.com>
2025-04-17 20:03:46 -0600

    refactorizando para enviarlo

--- leetcodeproblems/MergedSortedArray/TestSorterArraySteps.java
+++ leetcodeproblems/MergedSortedArray/TestSorterArraySteps.java
@@ -2,44 +2,45 @@ package leetcodeproblems.MergedSortedArray;
 // problem https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
 public class TestSorterArraySteps {
 
-   public static void  merge(int[] a, int[] b, int m, int n){
+    public static void  merge(int[] nums1, int[] nums2, int m, int n){
 
-    int nums1Size = m+n;
-    int i=0,j =0;
-        while(nums1Size>i){
-            //compare
+        if(m==0 && n==1){
+            System.out.println("["+ nums2[0] + "]");
+        }else if(m==1 && n==0){
+            System.out.println("["+ nums1[0] + "]");
+        }else{
+            int nums1Size = m+n;
+            int i=0,j =0;
+            while(nums1Size>i){
+                if(nums1[i]<nums2[j] && i==0){
+                    i++;
+                }else if( nums1[i]==nums2[j]){
+                    nums2[j] = nums1[i+1];
+                    nums1[i+1] = nums1[i];
+                    i=i+2;
+                }else if(nums2[j] > nums1[i] && i!=0){
+                    nums1[i]=nums2[j];
+                    j++;
+                    i++;
+                }
+            }
 
-            if(a[i]<b[j] && i==0){
-                i++;
-            }else if( a[i]==b[j]){
-                b[j] = a[i+1];
-                a[i+1] = a[i];
-                i=i+2;
-            }else if(b[j] > a[i] && i!=0){
-                a[i]=b[j];
-                j++;
-                i++;
+            for(int k=0; k<nums1.length; k++){
+                System.out.print("["+ nums1[k] + "]");
             }
         }
-   }
 
-   public static void main(String[] args) {
 
-       int[] foo={1};
-       int[] bar ={};
-       int m=1;
-       int n=0;
+    }
 
-       if(m==0 && n==1){
-           System.out.println("["+ bar[0] + "]");
-       }else if(m==1 && n==0){
-           System.out.println("["+ foo[0] + "]");
-       }else{
-           merge(foo,bar,m,n);
+    public static void main(String[] args) {
 
-           for(int i=0; i<foo.length; i++){
-               System.out.print("["+ foo[i] + "]");
-           }
-       }
-   }
+        int[] foo={1,2,3,0,0,0};
+        int[] bar ={2,5,6};
+        int m=3;
+        int n=3;
+        merge(foo,bar,m,n);
+
+
+    }
 }
